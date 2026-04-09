25d1aee8b575683c167d788fa07604dec313f6f4
King-BigDog <huntersmi95@gmail.com>
2026-04-09 13:12:30 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/lifev2/", 
+  base: "./", // Changed from "/lifev2/" to "./" to use relative paths
   server: {
     host: "::",
     port: 8080,
