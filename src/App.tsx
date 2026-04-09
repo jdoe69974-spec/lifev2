fe59b691bb12fd99dd987b7de7af242715ec9b4d
King-BigDog <huntersmi95@gmail.com>
2026-04-09 13:17:39 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* The basename tells the app it lives in the /lifev2 subfolder */}
+      <BrowserRouter basename="/lifev2">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
